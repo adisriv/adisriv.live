ba01fda9ec2666027e6d14bb52af81fb14e52be0
Aditya Srivastava <aditya12497@yahoo.com>
2020-06-11 22:14:40 -0400

    Updates

---
+++ precache-manifest.f2c16c4febf58a5c90d918ccd5298141.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d055bd1a1303880c79352d37bf37a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd41363dd308d113393",
+    "url": "/static/css/main.314fe966.chunk.css"
+  },
+  {
+    "revision": "1ff4c787d36a597372f8",
+    "url": "/static/js/2.3c74f364.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c74f364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd41363dd308d113393",
+    "url": "/static/js/main.68e60d4d.chunk.js"
+  },
+  {
+    "revision": "7aa08ec9adfbe3155d4a",
+    "url": "/static/js/runtime-main.7b3c6a9a.js"
+  }
+]);
