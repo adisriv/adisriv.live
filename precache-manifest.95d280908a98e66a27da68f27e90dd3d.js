0fd0c24393d5d1f794f623864c48739021bfd555
Aditya Srivastava <aditya12497@yahoo.com>
2020-06-11 21:46:56 -0400

    Updates

---
+++ precache-manifest.95d280908a98e66a27da68f27e90dd3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150ee277273971f8fecb37b2ce61ffc1",
+    "url": "/adisriv.live/index.html"
+  },
+  {
+    "revision": "c2fc2f0714e36bc5af53",
+    "url": "/adisriv.live/static/css/main.314fe966.chunk.css"
+  },
+  {
+    "revision": "1ff4c787d36a597372f8",
+    "url": "/adisriv.live/static/js/2.3c74f364.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adisriv.live/static/js/2.3c74f364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fc2f0714e36bc5af53",
+    "url": "/adisriv.live/static/js/main.253f0a53.chunk.js"
+  },
+  {
+    "revision": "51ab7b833ba3f118c92c",
+    "url": "/adisriv.live/static/js/runtime-main.bd94da2a.js"
+  }
+]);
